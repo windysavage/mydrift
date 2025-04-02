18c7677dfa7c3ddba39f9276a096715676d78a4d
windysavage <water92001@gmail.com>
2025-04-02 20:35:18 +0800

    add file

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    QDRANT_HOST: str
+    OLLAMA_HOST: str
+
+    class Config:
+        env_file = '.env'
+        case_sensitive = True
+
+
+settings = Settings()
